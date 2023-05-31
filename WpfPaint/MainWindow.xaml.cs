fe4d75205ab660fbc2eca63f522369c76e5aaf53
MegaSusl <Suslik_k@list.ru>
2023-06-01 01:46:32 +0300

    Looking Good mmmm... v1.0

--- WpfPaint/MainWindow.xaml.cs
+++ WpfPaint/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -23,6 +24,49 @@ namespace WpfPaint
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private readonly DrawingAttributes Pen = new DrawingAttributes()
+        {
+            Color = Colors.Red,
+            Height = 2,
+            Width = 2
+        };
+        private readonly DrawingAttributes Eraser1 = new DrawingAttributes()
+        {
+            Height = 50,
+            Width = 50
+        };
+        private void RowDefinition_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) { this.DragMove(); }
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void minimizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void closeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void PencilBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Canvas.EditingMode = InkCanvasEditingMode.Ink;
+            Canvas.DefaultDrawingAttributes = Pen;
+        }
+
+        private void EraserBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Canvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+            Canvas.DefaultDrawingAttributes = Eraser1;
         }
     }
 }
